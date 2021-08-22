0cd22d8fe6c11625fd6fbada13fd99c32725d8b6
David Franz <davidfranznz@gmail.com>
2021-08-22 15:55:53 +1200

    added notes

--- GA_KnapSack.py
+++ GA_KnapSack.py
@@ -1,3 +1,14 @@
 # maximum value
-def fitness_function():
+# possibly dictionaries will be not efficient- consider later
+def fitness_function(dict_of_chosen_items_to_values):
 	pass
+
+'''
+need to program:
+
+- random generation of initial generation
+
+- way of selecting individuals for next generation: _describe_method_
+
+- run for 5 generations
+'''
